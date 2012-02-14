c68581d4f7c997429396b8738fda35f75a0a514d
bonty <bonty.shushusha@gmail.com>
2012-02-14 14:15:19 +0900

    add some files

---
+++ LRUCacheTest.hpp
@@ -0,0 +1,31 @@
+
+#ifndef CPP_UNIT_EXAMPLETESTCASE_H
+#define CPP_UNIT_EXAMPLETESTCASE_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+/* 
+ * A test case that is designed to produce
+ * example errors and failures
+ *
+ */
+
+class LRUCacheTest : public CPPUNIT_NS::TestFixture
+{
+  CPPUNIT_TEST_SUITE( LRUCacheTest );
+  CPPUNIT_TEST( example );
+  CPPUNIT_TEST_SUITE_END();
+
+protected:
+  double m_value1;
+  double m_value2;
+
+public:
+  void setUp();
+
+protected:
+  void example();
+};
+
+
+#endif
