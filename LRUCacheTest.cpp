a4592bc8950b2151dad4773e9735a59e0fd12861
bonty <bonty.shushusha@gmail.com>
2012-02-14 13:34:59 +0900

    touch files

---
+++ LRUCacheTest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <cppunit/TestRunner.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+
+class LRUCacheTest : public CPPUNIT_NS::TestCase
+{
+  
+};
