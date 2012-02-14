3abbd9954614e7d4a99c03c0777db8cee69f2f37
bonty <bonty.shushusha@gmail.com>
2012-02-14 13:55:05 +0900

    edit header, testfile

---
+++ LRUCache.hpp
@@ -0,0 +1,14 @@
+class LRUCache
+{
+private:
+  int m_max;
+  std::vector<std::string> m_queue;
+  std::map<std::string, std::string> m_map;
+  
+public:
+  LRUCache();
+  ~LRUCache();
+
+  int put(const std::string key, const std::string value);
+  std::string get(const std::string key);
+}
--- LRUCacheTest.cpp
+++ LRUCacheTest.cpp
@@ -6,8 +6,37 @@
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/BriefTestProgressListener.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
+#include "LRUCache.hpp"
 
 class LRUCacheTest : public CPPUNIT_NS::TestCase
 {
+  CPPUNIT_TEST_SUITE(LRUCache);
+  // insert test method here
+  CPPUNIT_TEST_SUITE_END();
+
+private:
+  LRUCache *plru;
   
+public:
+  LRUCacheTest() {}
+
+  void setUp() {
+    *plru = new LRUCache();
+  }
+
+  void tearDown() {
+    delete plru;
+  }
+
+  // insert test method here
 };
+
+int main(void) {
+  CppUnit::TestSuite *suit = LRUCacheTest::suite();
+  CppUnit::TextUi::TestRunner runner;
+  runner.addTest(suite);
+  bool retcode = runner.run();
+  reeturn !retcode;
+}
